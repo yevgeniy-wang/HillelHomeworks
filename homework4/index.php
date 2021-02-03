63da4d7d11e795fffbd0006e23eb4200297fee38
yevge <yevgeniy@wang>
2021-02-03 02:06:40 +0200

    HW4(V2)

--- homework4/index.php
+++ homework4/index.php
@@ -13,13 +13,12 @@ echo 'Currency 2: ' . $currency2->getCode() . '<br><br>';
 
 echo 'Are they equal? ';
 if ($currency1->equals($currency2) == true) {
-    echo '- Yes, they are.' . '<br>'. '_____' . '<br><br>';
-} else echo '- No, they are not' . '<br>'. '_____' . '<br><br>';
+    echo '- Yes, they are.' . '<br>' . '_____' . '<br><br>';
+} else echo '- No, they are not' . '<br>' . '_____' . '<br><br>';
 
 
-
-echo 'Money 1: ' . $money1->getAmount() . $money1->getCurrency()->getCode(). '<br><br>';
-echo 'Money 2: ' . $money2->getAmount() . $money2->getCurrency()->getCode(). '<br><br>';
+echo 'Money 1: ' . $money1->getAmount() . $money1->getCurrency()->getCode() . '<br><br>';
+echo 'Money 2: ' . $money2->getAmount() . $money2->getCurrency()->getCode() . '<br><br>';
 
 echo 'Are they equal? ';
 if ($money1->equals($money2) == true) {
